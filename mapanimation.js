d9a53c8d4b31a36cbb099c07188e31044d1eded7
Ben Gross <benjamingro@gmail.com>
2021-04-08 13:53:16 -0500

    dual promise resolution, realized I only need to get the routes and not both routes and vehicles

--- mapanimation.js
+++ mapanimation.js
@@ -1,6 +1,6 @@
 angular.module('busTrackerApp', []).controller('BusTrackerCtrl', ['$scope', '$http', function ($scope, $http) {
   var self = this;
-
+  // #region mapbox members
   self.map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -9,109 +9,53 @@ angular.module('busTrackerApp', []).controller('BusTrackerCtrl', ['$scope', '$ht
     zoom: 12,
   });
 
-  self.marker = {};
-  self.routes = [];
-
-
-
+  // self.marker = {};
   // self.marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(self.map);
+  // #endregion
 
-  $http({
-    method: 'GET',
-    url: 'https://api-v3.mbta.com/routes?api_key=ac149cccb08b4e288b949e58d9eac162'
-  }).then(function successCallback(response) {
-    // console.log("succeeded! response = ");
-    // console.log(response);
-    self.routes = response.data.data; 
-    console.log("succeeded! self.routes = ");
-    console.log(self.routes);
+  self.routes = [];
+  self.vehicles = [];
+  self.mapBox_api_key = "ac149cccb08b4e288b949e58d9eac162";
 
-    // this callback will be called asynchronously
-    // when the response is available
-  }, function errorCallback(response) {
-    console.log("failed! response = ");
-    console.log(response);
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
+  self.initialize = function () {
 
-  // self.busStops = [
-  //   [-71.093729, 42.359244],
-  //   [-71.094915, 42.360175],
-  //   [-71.0958, 42.360698],
-  //   [-71.099558, 42.362953],
-  //   [-71.103476, 42.365248],
-  //   [-71.106067, 42.366806],
-  //   [-71.108717, 42.368355],
-  //   [-71.110799, 42.369192],
-  //   [-71.113095, 42.370218],
-  //   [-71.115476, 42.372085],
-  //   [-71.117585, 42.373016],
-  //   [-71.118625, 42.374863],
-  // ];
+    let routesPromise = $http({
+      method: 'GET',
+      url: 'https://api-v3.mbta.com/routes?api_key=' + self.mapBox_api_key
+    }).then((response) => {
+      self.routes = response.data.data;
+      console.log("self.routes = ");
+      console.log(self.routes);
+    }, (errorResponse) => {
+      // need to handle this error 
+    });
 
-  // self.counter = 0;
+    let vehiclesPromise = $http({
+      method: 'GET',
+      url: 'https://api-v3.mbta.com/vehicles?api_key=' + self.mapBox_api_key
+    }).then((response) => {
+      self.vehicles = response.data.data;
+      console.log("self.vehicles = ");
+      console.log(self.vehicles);
+    }, (errorResponse) => {
+      // need to handle this error 
+    });
 
-  // self.move = function() {
-  //   // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-  //   // Use counter to access bus stops in the array busStops
+    Promise.all([routesPromise, vehiclesPromise])
+      .then(
+        (response) => { 
+          console.log("all promises resolved! ");
+        },
+        (errorResponse) => { 
+          console.log("error in one of the promises ");
 
-  //   setTimeout(() => {
-  //     if (self.counter >= self.busStops.length) return;
-  //     self.marker.setLngLat(self.busStops[self.counter]);
-  //     self.counter++;
-  //     self.move();
-  //   }, 1000);
-  // }
+        });
+
+  };
+
+
+
+  self.initialize();
 
 }]);
 
-// // This array contains the coordinates for all bus stops between MIT and Harvard
-// const busStops = [
-//   [-71.093729, 42.359244],
-//   [-71.094915, 42.360175],
-//   [-71.0958, 42.360698],
-//   [-71.099558, 42.362953],
-//   [-71.103476, 42.365248],
-//   [-71.106067, 42.366806],
-//   [-71.108717, 42.368355],
-//   [-71.110799, 42.369192],
-//   [-71.113095, 42.370218],
-//   [-71.115476, 42.372085],
-//   [-71.117585, 42.373016],
-//   [-71.118625, 42.374863],
-// ];
-
-// // TODO: add your own access token
-// mapboxgl.accessToken =
-//   'pk.eyJ1IjoidGVzdHVzZXIxMDAwIiwiYSI6ImNraDkzZ2pkMzAzMHoycnBmMXpvZ3UwZnMifQ.jAE4YsPeAJv50VK92NSpOQ';
-
-// // TODO: create the map object using mapboxgl.map() function
-// let map = new mapboxgl.Map({
-//   container: 'map',
-//   style: 'mapbox://styles/mapbox/streets-v11',
-//   center: [-71.104081, 42.365554],
-//   zoom: 14,
-// });
-
-// // TODO: add a marker to the map
-// let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
-
-// // counter here represents the index of the current bus stop
-// let counter = 0;
-// function move() {
-//   // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-//   // Use counter to access bus stops in the array busStops
-
-//   setTimeout(() => {
-//     if (counter >= busStops.length) return;
-//     marker.setLngLat(busStops[counter]);
-//     counter++;
-//     move();
-//   }, 1000);
-// }
-
-// // // Do not edit code past this point
-// if (typeof module !== 'undefined') {
-//   module.exports = { move, counter, marker, busStops };
-// }
