f776710c08e56551ed45fd72dd1639accb069ddb
Ben Gross <benjamingro@gmail.com>
2021-04-09 07:03:35 -0500

    stable with list working

--- mapanimation.js
+++ mapanimation.js
@@ -11,11 +11,16 @@ angular.module('busTrackerApp', []).controller('BusTrackerCtrl', ['$scope', '$ht
 
   // self.marker = {};
   // self.marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(self.map);
+  self.geojson = {
+    type:'FeatureCollection',
+    features:[]
+  };
   // #endregion
 
   self.routes = [];
-  self.selectedRoutes = []; 
-  // self.vehicles = [];
+  self.activeRoutes = [];
+  self.selectedRoutes = [];
+  self.vehicles = [];
   self.mapBox_api_key = "ac149cccb08b4e288b949e58d9eac162";
 
   self.filterText = "";
@@ -30,36 +35,70 @@ angular.module('busTrackerApp', []).controller('BusTrackerCtrl', ['$scope', '$ht
 
       for (let i = 0; i < self.routes.length; i++) {
         self.routes[i]['selected'] = false;
+        self.routes[i]['vehicles'] = [];
       }
-      self.updateEventHandlers(); 
+
     }, (errorResponse) => {
       // need to handle this error 
       console.log("inside error");
     });
 
+    let vehiclesPromise = $http({
+      method: 'GET',
+      url: 'https://api-v3.mbta.com/vehicles?api_key=' + self.mapBox_api_key
+    }).then((response) => {
+      self.vehicles = response.data.data;
 
-    
+    }, (errorResponse) => {
+      // need to handle this error 
+      console.log("inside error");
+    });
+
+    Promise.all([routesPromise, vehiclesPromise]).then(
+      (responses) => {
+        let i, j = 0;
+
+        for (i = 0; i < self.routes.length; i++) {
+          for (j = 0; j < self.vehicles.length; j++) {
+            if (self.routes[i].id === self.vehicles[j].relationships.route.data.id) {
+              self.routes[i].vehicles.push(self.vehicles[j].id);
+            }
+          }
+
+        }
+
+        for (i = 0; i < self.routes.length; i++) {
+          if (self.routes[i].vehicles.length > 0) {
+            self.activeRoutes.push(self.routes[i]);
+          }
+        }
+        self.updateEventHandlers();
+
+      },
+      (errorResponses) => {
+      });
   };
 
   self.initialize();
 
-  self.updateSelectedRoutes = function()
-  {
-    self.selectedRoutes = []; 
-    for(let i=0; i<self.routes.length;i++ )
-    {
-      if(self.routes[i].selected == true)
-      {
-        self.selectedRoutes.push(self.routes[i].id); 
+  self.updateSelectedRoutes = function () {
+    self.selectedRoutes = [];
+    for (let i = 0; i < self.routes.length; i++) {
+      if (self.routes[i].selected == true) {
+        self.selectedRoutes.push(
+          {
+            routeId: self.routes[i].id,
+            vehicles: self.routes[i].vehicles
+          }
+        );
       }
     }
   }
 
-
-
   self.updateEventHandlers = function () {
-    console.log("inside updateEventHandlers "); 
-    setTimeout(function(){
+
+    setTimeout(function () {
+
       $('.clickArea').off("click");
 
       $('.clickArea').click(function (event) {
@@ -67,14 +106,15 @@ angular.module('busTrackerApp', []).controller('BusTrackerCtrl', ['$scope', '$ht
           $('input:checkbox', this).trigger('click');
         }
       });
-  
+
       $('.clickArea').hover(function () {
         $(this).addClass("hovered");
       }, function () {
         $(this).removeClass("hovered");
       });
-    },75)
-    
+      console.log("finish updateEventHandlers ");
+    }, 200)
+
   }
 
 }]);
