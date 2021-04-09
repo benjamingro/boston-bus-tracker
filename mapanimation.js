200ebe6760945b7ae7e453123f3f4e1e2fb1a3ea
Ben Gross <benjamingro@gmail.com>
2021-04-09 09:32:58 -0500

    commit before changing filtering mechanism

--- mapanimation.js
+++ mapanimation.js
@@ -12,15 +12,18 @@ angular.module('busTrackerApp', []).controller('BusTrackerCtrl', ['$scope', '$ht
   // self.marker = {};
   // self.marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(self.map);
   self.geojson = {
-    type:'FeatureCollection',
-    features:[]
+    type: 'FeatureCollection',
+    features: []
   };
   // #endregion
 
   self.routes = [];
   self.activeRoutes = [];
+  self.activeFilteredRoutes = [];
+
   self.selectedRoutes = [];
   self.vehicles = [];
+  self.updateIntervalMs = 3000;
   self.mapBox_api_key = "ac149cccb08b4e288b949e58d9eac162";
 
   self.filterText = "";
@@ -61,10 +64,14 @@ angular.module('busTrackerApp', []).controller('BusTrackerCtrl', ['$scope', '$ht
         for (i = 0; i < self.routes.length; i++) {
           for (j = 0; j < self.vehicles.length; j++) {
             if (self.routes[i].id === self.vehicles[j].relationships.route.data.id) {
-              self.routes[i].vehicles.push(self.vehicles[j].id);
+              self.routes[i].vehicles.push(
+                {
+                  vehicleId: self.vehicles[j].id,
+                  lngLatArray: []
+                }
+              );
             }
           }
-
         }
 
         for (i = 0; i < self.routes.length; i++) {
@@ -72,8 +79,10 @@ angular.module('busTrackerApp', []).controller('BusTrackerCtrl', ['$scope', '$ht
             self.activeRoutes.push(self.routes[i]);
           }
         }
-        self.updateEventHandlers();
 
+        self.updateActiveFilteredRoutes(); 
+        self.updateEventHandlers();
+        $scope.$apply();
       },
       (errorResponses) => {
       });
@@ -81,18 +90,64 @@ angular.module('busTrackerApp', []).controller('BusTrackerCtrl', ['$scope', '$ht
 
   self.initialize();
 
-  self.updateSelectedRoutes = function () {
-    self.selectedRoutes = [];
-    for (let i = 0; i < self.routes.length; i++) {
-      if (self.routes[i].selected == true) {
-        self.selectedRoutes.push(
-          {
-            routeId: self.routes[i].id,
-            vehicles: self.routes[i].vehicles
-          }
-        );
+  self.updateActiveFilteredRoutes = function()
+  {
+    
+  }
+
+  self.getSelectedVehiclesPosition = function () {
+    // console.log('inside self.getSelectedVehiclesPosition');
+    // console.log('self.selectedRoutes = ');
+    // console.log(self.selectedRoutes); 
+
+    let i, j = 0;
+    let positionPromiseArray = [];
+    for (i = 0; i < self.selectedRoutes.length; i++) {
+      for (j = 0; j < self.selectedRoutes[i].vehicles.length; j++) {
+        const my_url = `https://api-v3.mbta.com/vehicles/${self.selectedRoutes[i].vehicles[j].vehicleId}?api_key=${self.mapBox_api_key}`; 
+        console.log(`my_url = ${my_url}`);
+        let positionPromise = $http({
+          method: 'GET',
+          url: my_url
+        }).then(
+          (response) => {
+            const myLngLatArray = [response.data.data.attributes.longitude, response.data.data.attributes.latitude]
+            self.selectedRoutes[i].vehicles[j].lngLatArray = myLngLatArray;
+            console.log(`routeId = ${self.selectedRoutes[i].id} , vehicleId = ${self.selectedRoutes[i].vehicles[j].vehicleId} , myLngLatArray = ${JSON.stringify(myLngLatArray)}`);
+          },
+          (errorResponse) => {
+
+          });
+
+          positionPromiseArray.push(positionPromise); 
       }
     }
+
+    Promise.all(positionPromiseArray).then(
+      (responses)=>{
+        // update vehicle positions on map 
+      },
+      (errorResponses)=>{
+
+      });
+  }
+
+  setInterval(self.getSelectedVehiclesPosition,self.updateIntervalMs);
+
+  self.updateSelectedRoutes = function () {
+    self.selectedRoutes = [];
+    console.log('inside updateSelectedRoutes, self.activeRoutes.length = '+self.activeRoutes.length); 
+    let selectedCounter = 0; 
+    let i = 0
+    for (i = 0; i < self.activeRoutes.length; i++) {
+      if (self.activeRoutes[i].selected == true) {
+        selectedCounter++; 
+        self.selectedRoutes.push(self.activeRoutes[i]);
+      }
+    }
+    console.log('finished self.updateSelectedRoutes, self.activeRoutes = ');
+    console.log(self.activeRoutes) ; 
+    console.log(`selectedCounter = ${selectedCounter}`); 
   }
 
   self.updateEventHandlers = function () {
